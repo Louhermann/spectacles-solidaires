07fd81e6c12389894e9fd8d346efd1e0522d6176
raouuul <mat0195@hotmail.fr>
2022-05-03 14:50:47 +0200

    Components Home done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,25 @@
-// styles
+// Import styles
 import './home.scss';
-import testImg from '../../assets/images/Nekolo/Nekolo.jpg';
+
+// == Img
 
 function Home() {
   return (
     <div className="home">
-      <img className="home__picture" src={testImg} alt="test" />
-      <div className="home__description">
-        <h1 className="home__description--title ">Descritpion du site</h1>
-        <p className="home__description--text">
-          It is a long established fact that a reader will be distracted by the readable
-          content of a page when looking at its layout.
-          The point of using Lorem Ipsum is that it has a more-or-less normal distribution
-          of letters, as opposed to using 'Content here,
-          content here', making it look like readable English. Many desktop publishing
-          packages and web page editors now use Lorem Ipsum
-          as their default model text, and a search for 'lorem ipsum' will uncover many
-          web sites still in their infancy. Various versions
-          have evolved over the years, sometimes by accident, sometimes on purpose
-          (injected humour and the like).
-        </p>
+      <h1 className="home__title ">Descritpion du site</h1>
+      <p className="home__text">
+        " It is a long established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout.
+        The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+        of letters, as opposed to using 'Content here,
+        content here', making it look like readable English. Many desktop publishing
+        packages and web page editors now use Lorem Ipsum
+        as their default model text, and a search for 'lorem ipsum' will uncover many
+        web sites still in their infancy. Various versions
+        have evolved over the years, sometimes by accident, sometimes on purpose
+        (injected humour and the like). "
+      </p>
 
-      </div>
     </div>
   );
 }
