29b5db9b1d01c55bbf276616f2d428cf4aded8c7
Louhermann <hermann.lou@hotmail.fr>
2022-05-05 14:58:21 +0200

    catalog page created

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,14 +1,31 @@
-// == Import
+// == Import : local
+// Data
+import {catalogData} from './CatalogData';
+// Style
 import './catalog.scss';
 
 // == Composant
 function Catalog() {
+
   return (
-    <div className="">
-      Ici sera la page de tout les spectacles
+    <div className="catalog">
+      {catalogData.map(show => 
+        <>
+         <div className='catalog__show'>
+           <div className='catalog__show-poster'>{show.content}</div>
+           <h2 className='catalog__show-title' key={show.name}>{`${show.name}`}</h2> 
+         </div>
+        </>,
+      )}
     </div>
   );
 }
 
 // == Export
 export default Catalog;
+
+
+
+
+
+
