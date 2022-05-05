4efe1fe03a73b803b174981577d46f17f6b4ad30
Laurent Pichon <lrtpichon@gmail.com>
2022-05-05 21:28:35 +0200

    change css in BEM

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,59 +4,59 @@ import './contact.scss';
 function Contact() {
   return (
     <div className="card">
-      <h2>Contactez nous</h2>
-      <div className="row">
-        <div className="col">
-          <div className="form-group">
-            <label>Prenom</label>
-            <input name="prenom" type="text" />
+      <h2 className="card__title">Contactez nous</h2>
+      <div className="card__row">
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">Prenom</label>
+            <input className="card__input" name="prenom" type="text" />
           </div>
         </div>
 
-        <div className="col">
-          <div className="form-group">
-            <label>Nom</label>
-            <input name="nom" type="text" />
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">Nom</label>
+            <input className="card__input" name="nom" type="text" />
           </div>
         </div>
 
-        <div className="col">
-          <div className="form-group">
-            <label>E-mail</label>
-            <input name="mail" type="text" />
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">E-mail</label>
+            <input className="card__input" name="mail" type="text" />
           </div>
         </div>
 
-        <div className="col">
-          <div className="form-group">
-            <label>Téléphone</label>
-            <input type="tel" name="telephone" />
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">Téléphone</label>
+            <input className="card__input" type="tel" name="telephone" />
           </div>
         </div>
 
-        <div className="col">
-          <div className="form-group">
-            <label>Public</label>
-            <input type="text" name="age" />
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">Public</label>
+            <input className="card__input" type="text" name="age" />
           </div>
         </div>
 
-        <div className="col">
-          <div className="form-group">
-            <label>Budget</label>
-            <input type="text" name="prix" />
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">Budget</label>
+            <input className="card__input" type="text" name="prix" />
           </div>
         </div>
 
-        <div className="col">
-          <div className="form-group ">
-            <label>Message</label>
-            <textarea className="textarea" type="text" name="message" style={{ height: '7em', width: '90%' }} />
+        <div className="card__col">
+          <div className="card__form">
+            <label className="card__label">Message</label>
+            <textarea className="card__textarea" type="text" name="message" />
           </div>
         </div>
 
-        <div className="col">
-          <input type="submit" value="Envoyer" />
+        <div className="card__col">
+          <input className="card__input--submit" type="submit" value="Envoyer" />
         </div>
       </div>
     </div>
